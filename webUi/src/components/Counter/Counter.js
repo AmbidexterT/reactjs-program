1180e238261d1976dd76c298ba081fc1f536c13f
Stepan Petrov <jakeepping11@gmail.com>
2023-09-04 11:48:34 +0400

    refactoring

--- webUi/src/components/Counter/Counter.js
+++ webUi/src/components/Counter/Counter.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.initialValue || 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.initialValue || 0,
+        };
+    }
+
+    decrement = () => {
+        this.setState((prevState) => ({ value: prevState.value - 1 }));
     };
-  }
 
-  decrement = () => {
-    this.setState((prevState) => ({ value: prevState.value - 1 }));
-  };
+    increment = () => {
+        this.setState((prevState) => ({ value: prevState.value + 1 }));
+    };
 
-  increment = () => {
-    this.setState((prevState) => ({ value: prevState.value + 1 }));
-  };
-
-  render() {
-    return React.createElement('div', null,
-      React.createElement('h2', null, 'Counter'),
-      React.createElement('p', null, `Value: ${this.state.value}`),
-      React.createElement('button', { onClick: this.decrement }, 'Decrement'),
-      React.createElement('button', { onClick: this.increment }, 'Increment')
-    );
-  }
+    render() {
+        return React.createElement('div', null,
+            React.createElement('h2', null, 'Counter'),
+            React.createElement('p', null, `Value: ${this.state.value}`),
+            React.createElement('button', { onClick: this.decrement }, 'Decrement'),
+            React.createElement('button', { onClick: this.increment }, 'Increment')
+        );
+    }
 }
 
 export default Counter;
