27a21c7498661ae66afac27d09af7446d1d50507
Stepan Petrov <jakeepping11@gmail.com>
2023-09-11 18:02:23 +0400

    Task2. code refactoring

--- client/src/components/GenreList/GenreList.spec.tsx
+++ client/src/components/GenreList/GenreList.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import GenreList from './client/src/components/GenreList/GenreList';
+import GenreList from './GenreList';
 
 test('renders all genres passed in props', () => {
   const genres = ['Action', 'Adventure', 'Comedy'];
