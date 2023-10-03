f962d060e1f2e9e33be17e7427980b6e3cfcaf78
Stepan <stepan_petrov@epam.com>
2023-10-03 21:54:05 +0400

    Task5: add e2e tests for sorting

--- client/cypress/e2e/searchBar.cy.tsx
+++ client/cypress/e2e/searchBar.cy.tsx
@@ -1,20 +1,19 @@
-
 describe('SearchBar Component', () => {
-    let initialQuery: string;
-    beforeEach(() => {
-        cy.visit('http://localhost:3000');
+  let initialQuery: string;
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
 
-        initialQuery = 'Titanic';
-    });
+    initialQuery = 'Titanic';
+  });
 
-    it('should render with initial query', () => {
-        cy.get('.search-input').should('have.value', initialQuery);
-    });
+  it('should render with initial query', () => {
+    cy.get('.search-input').should('have.value', initialQuery);
+  });
 
-    it('should update the query input', () => {
-        const newQuery = '2';
-        cy.get('.search-input')
-            .type(newQuery)
-            .should('have.value', initialQuery+newQuery);
-    });
+  it('should update the query input', () => {
+    const newQuery = '2';
+    cy.get('.search-input')
+      .type(newQuery)
+      .should('have.value', initialQuery + newQuery);
+  });
 });
