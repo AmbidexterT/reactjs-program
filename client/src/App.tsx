faa049a7262520337822bac7c9ac6489d9dd5799
Stepan <stepan_petrov@epam.com>
2023-10-29 11:42:31 +0400

    Task8: add SSR

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,17 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ErrorBoundary from 'containers/ErrorBoundary';
 import AppRoutes from './routes';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 const App = () => (
-  <ErrorBoundary>
-    <Router>
-      <AppRoutes />
-    </Router>
-  </ErrorBoundary>
+  <Provider store={store}>
+    <ErrorBoundary>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </ErrorBoundary>
+  </Provider>
 );
 
 export default App;
